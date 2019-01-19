545f277707d0ea689c6b49dab3596ff8784a8e38
doman89 <mateusz.domanski.1989@gmail.com>
2019-01-19 18:50:12 +0100

    Add VehiclesLiElements component and use it in LiElement component. User now can searching vehicles on website.

--- src/components/LiElement.js
+++ src/components/LiElement.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PeopleLiElement from './PeopleLiElement';
 import StarshipsLiElement from './StarshipsLiElement';
 import PlanetsLiElement from './PlanetsLiElement';
+import VehiclesLiElement from './VehiclesLiElement';
 
 const LiElement = props =>{
 
@@ -10,7 +11,7 @@ const LiElement = props =>{
             case 'people': return <PeopleLiElement responseData={props.responseData}/>;
             case 'planets': return <PlanetsLiElement responseData={props.responseData}/>;
             case 'starships': return <StarshipsLiElement responseData={props.responseData}/>;
-            case 'vehicles': break;
+            case 'vehicles': return <VehiclesLiElement responseData={props.responseData} />;
             case 'films': break;
             case 'species': break;
             default: return null;
---
+++ src/components/VehiclesLiElement.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const VehiclesLiElement = props => {
+    return (
+        <li>
+            <h1>{props.responseData.name}</h1>
+            <p>Cargo capacity: {props.responseData.cargo_capacity}</p>
+            <p>Consumables: {props.responseData.consumables}</p>
+            <p>Cost in credits: {props.responseData.cost_in_credits}</p>
+            <p>Crew: {props.responseData.crew}</p>
+            <p>Length: {props.responseData.length}</p>
+            <p>Manufacturer: {props.responseData.manufacturer}</p>
+            <p>Maximum speed in the atmoshere: {props.responseData.max_atmosphering_speed}</p>
+            <p>Model: {props.responseData.model}</p>
+            <p>Passengers: {props.responseData.passengers}</p>
+            <p>Vehicle class: {props.responseData.vehicle_class}</p>
+        </li>
+    );
+};
+
+export default VehiclesLiElement;
