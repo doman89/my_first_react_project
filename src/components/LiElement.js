84f00b2598ed9e86460096ed6f23cc415a7d052a
doman89 <mateusz.domanski.1989@gmail.com>
2019-01-19 18:37:42 +0100

    Add PlanetsLiElements component and use it in LiElement component. User now can searching planets on website.

--- src/components/LiElement.js
+++ src/components/LiElement.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PeopleLiElement from './PeopleLiElement';
 import StarshipsLiElement from './StarshipsLiElement';
+import PlanetsLiElement from './PlanetsLiElement';
 
 const LiElement = props =>{
 
     const putRightLiElement = (props) => {
         switch (props.getTypeItem) {
             case 'people': return <PeopleLiElement responseData={props.responseData}/>;
-            case 'planets': break;
+            case 'planets': return <PlanetsLiElement responseData={props.responseData}/>;
             case 'starships': return <StarshipsLiElement responseData={props.responseData}/>;
             case 'vehicles': break;
             case 'films': break;
---
+++ src/components/PlanetsLiElement.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const PlanetsLiElement = props => {
+    return (
+      <li>
+          <h1>{props.responseData.name}</h1>
+          <p>Climate: {props.responseData.climate}</p>
+          <p>Diameter: {props.responseData.diameter}</p>
+          <p>Gravity: {props.responseData.gravity}</p>
+          <p>Orbital period: {props.responseData.orbital_period}</p>
+          <p>Rotation period: {props.responseData.rotation_period}</p>
+          <p>Population: {props.responseData.population}</p>
+          <p>Surface water: {props.responseData.surface_water}</p>
+          <p>Terrain: {props.responseData.terrain}</p>
+      </li>
+    );
+};
+
+export default PlanetsLiElement;
