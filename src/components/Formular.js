8280e0d9bb32bcaf60535eff70f95948b127fe45
doman89 <mateusz.domanski.1989@gmail.com>
2019-01-20 20:20:54 +0100

    Added support for 'Enter' key for input field.

--- src/components/Formular.js
+++ src/components/Formular.js
@@ -6,11 +6,16 @@ const Formular = (props) => {
       event.preventDefault();
     };
 
+    const handleKeyPress = event => {
+      if(event.key === 'Enter')
+          props.sendRequest();
+    };
+
     return (
       <div>
           <form onSubmit={handleFormSubmit}>
               <FormButtonField setTypeItem={props.setTypeItem}/>
-              <input type="text" placeholder="Put name to search" onInput={props.setNameItem}/>
+              <input type="text" placeholder="Put name to search" onInput={props.setNameItem} onKeyPress={handleKeyPress}/>
               <button onClick={props.sendRequest}>Find!</button>
           </form>
       </div>
